51466e7fa02b5b67d85cacb5ab4eb2e5aaac4688
callumlu <callumlu@gmail.com>
2019-05-10 17:33:37 +0800

    fixed some synatx problem

--- do_delete.php
+++ do_delete.php
@@ -4,27 +4,29 @@ session_start();
 
 $config = json_decode(file_get_contents('_config.json'));
 
-$file =  $_POST['file'];
-$file = urldecode($file);
+$file = urldecode($_POST['file']);
 
+$target = $config->upload_root;
 
-$target=$config->upload_root;
-
-if ($_SESSION['pwd']!='/')
+if ($_SESSION['pwd']!='/') {
     $target = $target.$_SESSION['pwd'].'/';
+}
+
 $target.=$file;
 
 echo $target;
 
 if (file_exists($target)) {
-    if (is_dir($target))
+    if (is_dir($target)) {
         rmdir($target);
-    else
+    }
+    else {
         unlink($target);
+    }
         
     $_SESSION['delete_result'] = 'success';
 }
-else
+else {
     $_SESSION['delete_result'] = 'failed';
-
+}
 ?>
