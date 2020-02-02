e90c5971842e1b80e802f28b0575eefb87f80309
Callum Lu <610721230@gms.ndhu.edu.tw>
2020-02-02 15:00:25 +0000

    mirror bugs fixed

--- remain.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('API_URL', 'https://api.telegram.org/bot868385679:AAHea69gcXkC19t85sCx7BUbgFhWWCqpdQc/');
-
-$sendto =API_URL."sendmessage?chat_id=460873343&text=".$argv[1];
-file_get_contents($sendto);
-?>
